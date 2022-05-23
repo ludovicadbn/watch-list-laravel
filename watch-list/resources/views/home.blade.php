2bd02a594ce53ba28f02de934e9fba840acb1f23
dibss <ludovicad01@gmail.com>
2022-05-23 12:31:24 +0200

    laravel files

---
+++ watch-list/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Watch List</title>
+</head>
+<body>
+
+  <header>
+    <nav>
+      <a href="{{ route('home') }}"></a>
+      <a href="{{ route('modifylist') }}"></a>
+      <a href="{{ route('random') }}"></a>
+    </nav>
+  </header>
+  
+</body>
+</html>
